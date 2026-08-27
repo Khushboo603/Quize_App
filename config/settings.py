f5eaada78ff3daf0ae62d8e0be240b4427fc6581
Khushboo Kumari <khushboo.kumari@nusummit.com>
2026-08-27 12:57:26 +0530

    Prepare Docker app for production with Gunicorn

--- config/settings.py
+++ config/settings.py
@@ -26,12 +26,20 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s4ig_+6ch66d%uw-sr#4kqvzoik&l1r&-10(vn7=m!8eyicv2w'
+# SECRET_KEY = 'django-insecure-s4ig_+6ch66d%uw-sr#4kqvzoik&l1r&-10(vn7=m!8eyicv2w'
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 
 # Application definition
@@ -173,5 +181,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
